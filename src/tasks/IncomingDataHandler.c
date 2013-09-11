b5d951131dab1317a14aafdf08817b26ba283a38
Tobias <tbraun@inf>
2013-09-12 01:40:13 +0200

    Ringbuffer update

--- src/tasks/IncomingDataHandler.c
+++ src/tasks/IncomingDataHandler.c
@@ -21,20 +21,104 @@
 volatile int threads = 0;
 #define MAXTHREADS	(2)
 
-typedef struct IncomingDataHandler_t{
-  struct netconn *connection;
-  ringbuf_t incomingRingBuffer;
-}IncomingDataHandler_t;
+typedef enum ParseState_t {
+     init, preambleFound, headerFound
+} ParseState_t;
 
-void IncomingDataHandler_parseFrame(IncomingDataHandler_t* threadState){
+typedef struct IncomingDataHandler_t {
+     struct netconn *connection;
+     ringbuf_t incomingRingBuffer;
+     ParseState_t state;
+
+     uint8_t length, type, component;
+} IncomingDataHandler_t;
+
+void IncomingDataHandler_frameFound(uint8_t type, uint8_t component, uint8_t* payload, size_t len){
+}
+
+void IncomingDataHandler_parseFrame ( IncomingDataHandler_t* threadState )
+{
+     uint8_t tmp;
+     ringbuf_t* dataBuffer = & ( threadState->incomingRingBuffer );
+     for ( ;; ) {
+          switch ( threadState->state ) {
+          case init: {
+               // search preamble
+               while ( dataBuffer->len >= 2 ) {
+                    if ( rb_peek ( dataBuffer, 0 ) == ( 0xAB )
+                              && rb_peek ( dataBuffer, 1 ) == ( 0xCD ) ) {
+                         threadState->state = preambleFound;
+                         break;
+                    } else {
+                         // remove one byte and try again to match preamble
+                         rb_getc ( dataBuffer, &tmp );
+                    }
+               }
+               // wait for more data
+               if ( threadState->state != preambleFound )
+                    return;
+          }
+          case preambleFound: {
+               // wait for more data
+               if ( dataBuffer->len < ( 4 + 2 ) )
+                    return;
+               // interpret header
+               threadState->length = rb_peek ( dataBuffer, 2 );
+               threadState->type = rb_peek ( dataBuffer, 3 );
+               threadState->component = rb_peek ( dataBuffer,4 );
+
+               // check valid header format
+               if ( rb_peek ( dataBuffer, 5 ) == ( 0xFF ) ) {
+                    threadState->state = headerFound;
+               } else {
+                    // preamble seems to be not valid, so try again with
+                    // next one
+                    rb_getc ( dataBuffer, &tmp );
+                    threadState->state = init;
+                    break;
+               }
+          }
+          case headerFound: {
+               // wait for more data
+               if ( dataBuffer->len < threadState->length )
+                    return;
+	       
+               // look for end
+               if ( rb_peek ( dataBuffer,threadState->length - 2 ) == ( 0xEF )
+                         && rb_peek ( dataBuffer, threadState->length - 1 ) == ( 0xFE ) ) {
+                    
+		    // remove prefix and header
+		    for(int i=0; i< 6; i++)
+		      rb_getc ( dataBuffer, &tmp );
+		    
+		    // copy payload and construct frame
+		    size_t len = threadState->length - 8;
+		    uint8_t* payload = malloc(len);
+		    rb_read(dataBuffer, payload, len);
+                    IncomingDataHandler_frameFound( threadState->type, threadState->component, payload, len);
+
+                    // remove trailer from buffer
+                    for ( int i = 0; i < 2; i++ )
+                         rb_getc ( dataBuffer, &tmp );
+               } else {
+                    // something went wrong, retry
+                    rb_getc ( dataBuffer, &tmp );
+                    threadState->state = init;
+                    break;
+               }
+          }
+          default:
+               printf ( "WTF unknown state\n" );
+          }
+     }
 }
 
 /*-----------------------------------------------------------------------------------*/
 void IncomingDataHandler_thread ( void *arg )
 {
-     IncomingDataHandler_t* lArg = (IncomingDataHandler_t*) arg;
+     IncomingDataHandler_t* lArg = ( IncomingDataHandler_t* ) arg;
      struct netconn *connection = lArg->connection;
-     
+
      err_t xErr;
 
      struct netbuf *buf;
@@ -44,22 +128,22 @@ void IncomingDataHandler_thread ( void *arg )
      while ( ( xErr = netconn_recv ( connection, &buf ) ) == ERR_OK ) {
           do {
                netbuf_data ( buf, &data, &len );
-               netconn_write ( connection, data, len, NETCONN_COPY );
-	       rb_write(&lArg->incomingRingBuffer, (const uint8_t *) data, len);
+               //netconn_write ( connection, data, len, NETCONN_COPY );
+               rb_write ( & ( lArg->incomingRingBuffer ), ( const uint8_t * ) data, len );
           } while ( netbuf_next ( buf ) >= 0 );
 
           netbuf_delete ( buf );
-	  
-	  // try to parse frame
-	  IncomingDataHandler_parseFrame(lArg);
+
+          // try to parse frame
+          IncomingDataHandler_parseFrame ( lArg );
      }
 
      /* Close connection and discard connection identifier. */
      netconn_close ( connection );
      netconn_delete ( connection );
-     rb_free(&(lArg->incomingRingBuffer));
-     free(lArg);
-     
+     rb_free ( & ( lArg->incomingRingBuffer ) );
+     free ( lArg );
+
      vPortEnterCritical();
      --threads;
      vPortExitCritical();
@@ -71,10 +155,10 @@ bool NewIncomingDataHandlerTask ( void* connection )
      bool result = false;
      vPortEnterCritical();
      if ( threads < MAXTHREADS ) {
-	  IncomingDataHandler_t* threadState = malloc(sizeof(IncomingDataHandler_t));
-	  threadState->connection = connection;
-	  rb_alloc(&(threadState->incomingRingBuffer), 512);
-       
+          IncomingDataHandler_t* threadState = malloc ( sizeof ( IncomingDataHandler_t ) );
+          threadState->connection = connection;
+          rb_alloc ( & ( threadState->incomingRingBuffer ), 512 );
+
           xTaskCreate ( IncomingDataHandler_thread, ( const signed char * const ) "IncomingData",
                         configMINIMAL_STACK_SIZE, threadState, TCPINCOMINGDATAHandler_TASK_PRIO, NULL );
           result =  true;
